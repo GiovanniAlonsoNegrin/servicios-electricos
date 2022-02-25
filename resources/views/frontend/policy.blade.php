0ef46580296179abd43895d3d44a3fc7ad4f35ed
Giovanni Alonso <74564389+GiovanniAlonsoNegrin@users.noreply.github.com>
2022-02-25 13:10:23 +0000

    privacy_policy

--- resources/views/frontend/policy.blade.php
+++ resources/views/frontend/policy.blade.php
@@ -53,7 +53,51 @@
             <p>Tienes derecho a la tutela judicial efectiva y a presentar una reclamación ante la autoridad de control, en este caso, la Agencia Española de Protección de Datos, si consideras que el tratamiento de datos personales que te conciernen infringe el Reglamento.</p>
 
             <h1 class="text-3xl font-bold my-5">Finalidad del tratamiento de datos personales</h1>
-            <p></p>
+            <p>Cuando te conectas al sitio Web para mandar un correo al Titular, te suscribes a su boletín o realizas una contratación, estás facilitando información de carácter personal de la que el responsable es el Titular. Esta información puede incluir datos de carácter personal como pueden ser tu dirección IP, nombre y apellidos, dirección física, dirección de correo electrónico, número de teléfono, y otra información. Al facilitar esta información, das tu consentimiento para que tu información sea recopilada, utilizada, gestionada y almacenada por superadmin.es , sólo como se describe en el Aviso Legal y en la presente Política de Privacidad.</p>
+            <p>Los datos personales y la finalidad del tratamiento por parte del Titular es diferente según el sistema de captura de información:</p>
+            <p>ELIMINA LOS QUE NO USES:</p>
+            <ul class="ml-5 myPolicyListUl">
+                <li class="myPolicyListLi"><span class="font-bold">Formularios de contacto:</span> El Titular solicita datos personales entre los que pueden estar: Nombre y apellidos, dirección de correo electrónico, número de teléfono y dirección de tu sitio Web con la finalidad de responder a tus consultas.Por ejemplo, el Titular utiliza esos datos para dar respuesta a tus mensajes, dudas, quejas, comentarios o inquietudes que puedas tener relativas a la información incluida en el sitio Web, los servicios que se prestan a través del sitio Web, el tratamiento de tus datos personales, cuestiones referentes a los textos legales incluidos en el sitio Web, así como cualquier otra consulta que puedas tener y que no esté sujeta a las condiciones del sitio Web o de la contratación.<br></li>
+                <li class="myPolicyListLi"><span class="font-bold">Formularios de suscripción a contenidos:</span> El Titular solicita los siguientes datos personales: Nombre y apellidos, dirección de correo electrónico, número de teléfono y dirección de tu sitio web para gestionar la lista de suscripciones, enviar boletines, promociones y ofertas especiales.<br>Los datos que facilites al Titular estarán ubicados en los servidores de The Rocket Science Group LLC d/b/a, con domicilio en EEUU. (Mailchimp).</li>
+            </ul>
+            <p>Existen otras finalidades por las que el Titular trata tus datos personales:</p>
+            <ul class="ml-5 myPolicyListUl">
+                <li class="myPolicyListLi">Para garantizar el cumplimiento de las condiciones recogidas en el Aviso Legal y en la ley aplicable. Esto puede incluir el desarrollo de herramientas y algoritmos que ayuden a este sitio Web a garantizar la confidencialidad de los datos personales que recoge.</li>
+                <li class="myPolicyListLi">Para apoyar y mejorar los servicios que ofrece este sitio Web.</li>
+                <li class="myPolicyListLi">Para analizar la navegación. El Titular recoge otros datos no identificativos que se obtienen mediante el uso de cookies que se descargan en tu ordenador cuando navegas por el sitio Web cuyas caracterísiticas y finalidad están detalladas en la Política de Cookies.</li>
+                <li class="myPolicyListLi">OPara gestionar las redes sociales. el Titular tiene presencia en redes sociales. Si te haces seguidor en las redes sociales del Titular el tratamiento de los datos personales se regirá por este apartado, así como por aquellas condiciones de uso, políticas de privacidad y normativas de acceso que pertenezcan a la red social que proceda en cada caso y que has aceptado previamente.</li>
+            </ul>
+            <p>Puedes consultar las políticas de privacidad de las principales redes sociales en estos enlaces:</p>
+            <p>ELIMINA LOS QUE NO USES:</p>
+            <ul class="ml-5 myPolicyListUl">
+                <li class="myPolicyListLi">Facebook</li>
+                <li class="myPolicyListLi">Twitter</li>
+                <li class="myPolicyListLi">Linkedin</li>
+                <li class="myPolicyListLi">YouTube</li>
+                <li class="myPolicyListLi">Instagram</li>
+            </ul>
+            <p>El Titular tratará tus datos personales con la finalidad de administrar correctamente su presencia en la red social, informarte de sus actividades, productos o servicios, así como para cualquier otra finalidad que las normativas de las redes sociales permitan.</p>
+            <p>En ningún caso el Titular utilizará los perfiles de seguidores en redes sociales para enviar publicidad de manera individual.</p>
+
+            <h1 class="text-3xl font-bold my-5">Seguridad de los datos personales</h1>
+            <p>Para proteger tus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o destrucción de los mismos.</p>
+            <p>El sitio Web está alojado en PROVEEDOR DE ALOJAMIENTO WEB. La seguridad de tus datos está garantizada, ya que toman todas las medidas de seguridad necesarias para ello. Puedes consultar su política de privacidad para tener más información.</p>
+
+            <h1 class="text-3xl font-bold my-5">Contenido de otros sitios web</h1>
+            <p>Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.</p>
+            <p>Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar tu interacción usando este código.</p>
+
+            <h1 class="text-3xl font-bold my-5">Política de Cookies</h1>
+            <p>Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en tu navegador web.</p>
+            <p>En la página Política de Cookies puedes consultar toda la información relativa a la política de recogida, la finalidad y el tratamiento de las cookies.</p>
+
+            <h1>Legitimación para el tratamiento de datos</h1>
+            <p>La base legal para el tratamiento de tus datos es: el consentimiento.</p>
+            <p>Para contactar con el Titular, suscribirte a un boletín o realizar comentarios en este sitio Web tienes que aceptar la presente Política de Privacidad.</p>
+
+            <h1>Legitimación para el tratamiento de datos</h1>
+            <p>La base legal para el tratamiento de tus datos es: el consentimiento.</p>
+            <p>Para contactar con el Titular, suscribirte a un boletín o realizar comentarios en este sitio Web tienes que aceptar la presente Política de Privacidad.</p>
 
         </div>
     </div>
